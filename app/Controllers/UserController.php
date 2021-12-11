60372105d4aa5e7e0f8f3d49b9608a3e1f4aa41c
William R <wiribasczky@gmail.com>
2021-12-11 14:58:08 -0300

    Melhoria dos exemplos de model
    - Melhoria no exemplo de models com
      paginação

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Models\User;
+use Core\DB\Connect;
 use Core\DB\DB;
 use Services\Pager;
 
@@ -10,10 +11,10 @@ class UserController extends BaseController
 {
     public function index()
     {
-        $search = filter_input(INPUT_GET,"search",FILTER_SANITIZE_STRIPPED) ?? "";
-        $page = filter_input(INPUT_GET,"page",FILTER_VALIDATE_INT) ?? 1;
+        $search = filter_input(INPUT_GET, "search", FILTER_SANITIZE_STRIPPED) ?? "";
+        $page = filter_input(INPUT_GET, "page", FILTER_VALIDATE_INT) ?? 1;
         $query = DB::table("users");
-        if(!empty($search) && str_search($search) !== "all"){
+        if (!empty($search) && str_search($search) !== "all") {
             $search = str_search($search);
             $query->where("users.first_name LIKE :search")->params(['search' => "%{$search}%"]);
         }
@@ -31,23 +32,18 @@ class UserController extends BaseController
 
     public function users()
     {
-        $search = filter_input(INPUT_GET,"search",FILTER_SANITIZE_STRIPPED) ?? "";
-        $page = filter_input(INPUT_GET,"page",FILTER_VALIDATE_INT) ?? 1;
-        $query = DB::table("users");
-        if(!empty($search) && str_search($search) !== "all"){
-            $search = str_search($search);
-            $query->where("users.first_name LIKE :search")->params(['search' => "%{$search}%"]);
-        }
+        $search = filter_input(INPUT_GET, "search", FILTER_SANITIZE_STRIPPED) ?? "";
+        $page = filter_input(INPUT_GET, "page", FILTER_VALIDATE_INT) ?? 1;
 
-        $this->responseJSON([
-            "t" => (new User())->list(),
-        ]);
+        $usersModel = new User();
 
-        $total = $query->count();
+        $total = $usersModel->paginatedUsersTotal($search);
+
         $pager = new Pager(url("/users?search={$search}&page="));
         $pager->pager($total, 5, (!empty($page) ? $page : 1));
 
-        $users = $query->limit($pager->limit())->offset($pager->offset())->get();
+        $users = $usersModel->paginatedUsers($search, $pager->limit(), $pager->offset());
+
         $this->responseJSON([
             "pages" => $pager->pages(),
             "total" => $total,
@@ -59,7 +55,7 @@ class UserController extends BaseController
     {
         $id = filter_var($data["id"], FILTER_VALIDATE_INT);
         $user = DB::table("users")->where("id = :id")->params(["id" => $id])->first();
-        if(empty($user)){
+        if (empty($user)) {
             $this->responseJSON([
                 "user" => null,
                 "error" => "user_not_found"
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Core\DB\DB;
 use Core\DB\Model;
+use Services\Pager;
 
 class User extends Model
 {
@@ -21,7 +22,7 @@ class User extends Model
      */
     public function list($limit = 10, $offset = 0)
     {
-        return $this->db->select(["id","first_name","email","status"])->limit($limit)->offset($offset)->get();
+        return $this->db->select(["id", "first_name", "email", "status"])->limit($limit)->offset($offset)->get();
     }
 
     /**
@@ -32,6 +33,26 @@ class User extends Model
      */
     public function listAll()
     {
-        return DB::table($this->table)->select(["id","first_name","email","status"])->get();
+        return DB::table($this->table)->select(["id", "first_name", "email", "status"])->get();
+    }
+
+    public function paginatedUsers($search = "", $limit = 10, $offset = 0)
+    {
+        if (!empty($search) && str_search($search) !== "all") {
+            $search = str_search($search);
+            $this->db->where("users.first_name LIKE :search")->params(['search' => "%{$search}%"]);
+        }
+
+        return $this->db->select(["id", "first_name", "email", "status"])->limit($limit)->offset($offset)->get();
+    }
+
+    public function paginatedUsersTotal($search = "")
+    {
+        if (!empty($search) && str_search($search) !== "all") {
+            $search = str_search($search);
+            $this->db->where("users.first_name LIKE :search")->params(['search' => "%{$search}%"]);
+        }
+
+        return $this->db->count();
     }
 }
